42b93b4951b96afa78379a5c24e9b4e8e2866e65
hamzalimouni <hamzalimouni9@gmail.com>
2024-12-07 01:07:30 +0100

    start offer design

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,7 +1,15 @@
+import Image from "next/image"
 
 const Offer = () => {
     return (
-        <div>Offer</div>
+        <div className="">
+            {/* TEXT CONTAINER  */}
+            <div className=""></div>
+            {/* IMAGE CONTAINER  */}
+            <div className="">
+                <Image src="/offerProduct.png" alt="Offer product image" fill />
+            </div>
+        </div>
     )
 }
 
