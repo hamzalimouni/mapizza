60bd21f6e6503634b97ba94fb51fbc5a8d176819
hamzalimouni <hamzalimouni9@gmail.com>
2024-12-07 01:04:54 +0100

    finish featured design

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,3 +1,4 @@
+import { featuredProducts } from "@/data"
 import Image from "next/image"
 
 const Featured = () => {
@@ -6,19 +7,21 @@ const Featured = () => {
             {/* WRAPPER  */}
             <div className="w-max flex">
                 {/* SINGLE ITEM  */}
-                <div className="w-screen h-[60vh] flex flex-col items-center justify-around p-4">
-                {/* IMAGE CONTAINER  */}
-                <div className="flex-1 w-full relative">
-                    <Image src="/temporary/p1.png" alt="featured image" fill className="object-contain" />
-                </div>
-                {/* TEXT CONTAINER  */}
-                <div className="flex-1 flex flex-col gap-4">
-                    <h1 className="text-xl font-bold uppercase">Title</h1>
-                    <p>Desc</p>
-                    <span>123</span>
-                    <button className="bg-red-500 text-white p-2 rounded-md">Add To Cart</button>
-                </div>
-                </div>
+                {featuredProducts.map((item) =>
+                    <div key={item.id} className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]">
+                        {/* IMAGE CONTAINER  */}
+                        {item.img && <div className="flex-1 w-full relative hover:rotate-[60deg] transition-all duration-500">
+                            <Image src={item.img} alt="featured image" fill className="object-contain" />
+                        </div>}
+                        {/* TEXT CONTAINER  */}
+                        <div className="flex-1 flex flex-col items-center justify-center text-center gap-4">
+                            <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
+                            <p className="p-4 2xl:p-8">{item?.desc}</p>
+                            <span>${item.price}</span>
+                            <button className="bg-red-500 text-white p-2 rounded-md">Add To Cart</button>
+                        </div>
+                    </div>
+                )}
             </div>
         </div>
     )
